05f13fd17265f76f826a9cd9eca012230ad0621f
chuxin <15115359055@139.com>
2025-02-28 09:34:54 +0800

    feat: 实现vue3和主服务的搭建

---
+++ packages/main/src/components/SubContainer/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="sub-container"></div>
+</template>
+
+<script>
+import { start } from 'qiankun'
+import { registerApps } from '@/utils/qiankun'
+
+export default {
+  mounted() {
+    if (!window.qiankunStarted) {
+      window.qiankunStarted = true
+      registerApps()
+      start({
+        sandbox: {
+          experimentalStyleIsolation: true // 样式隔离
+        }
+      })
+    }
+  }
+}
+</script>
---
+++ packages/main/src/router/index.js
@@ -0,0 +1,39 @@
+import {
+    createRouter,
+    createWebHistory
+} from 'vue-router'
+import { qiankunWindow } from 'vite-plugin-qiankun/dist/helper'
+
+
+const routes = [
+    {
+        path: '',
+        redirect: { name: 'home' },
+        meta: { title: '首页' },
+        children: [
+            {
+                path: '/home',
+                name: 'home',
+                component: () => import('@/pages/home/index.vue')
+            },
+            {
+                // history模式需要通配所有路由，详见vue-router文档
+                path: '/app/app-vue3/:pathMatch(.*)*',
+                name: 'app-vue3',
+                component: () => import('@/components/SubContainer/index.vue')
+            },
+        ]
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(
+        qiankunWindow.__POWERED_BY_QIANKUN__
+            ? '/app/app-vue3/'
+            : '/'
+    ),
+    routes
+})
+
+
+export default router
