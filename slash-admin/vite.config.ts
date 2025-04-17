0f168139020d1cc58d8749674432d0eeb66cb1ec
chuxin <15115359055@139.com>
2025-04-17 17:04:30 +0800

    feat(assets): 新增多个SVG图标并配置Vite插件
    新增了多个SVG图标文件，包括搜索、箭头、仪表盘等常用图标。同时，在`vite.config.ts`中配置了`vite-plugin-svg-icons`插件，用于自动加载和管理SVG图标资源。

--- slash-admin/vite.config.ts
+++ slash-admin/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    createSvgIconsPlugin({
+      // 指定需要缓存的图标文件夹
+      iconDir: [path.resolve(process.cwd(), 'src/assets/icons')],
+      // 指定symbolId格式
+      symbolId: 'icon-[dir]-[name]',
+    }),
+  ],
+});
