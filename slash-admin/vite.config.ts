d76ba990b30579ff46163b432fd31a5b98644d2e
chuxin <15115359055@139.com>
2025-04-18 15:02:35 +0800

    refactor(vite.config): 优化配置代码格式并添加模式日志
    调整了代码格式以提高可读性，并在配置中添加了模式日志以便调试

--- slash-admin/vite.config.ts
+++ slash-admin/vite.config.ts
@@ -3,19 +3,21 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
 
-export default defineConfig(()=>{
+
+export default defineConfig(({ mode }) => {
+  console.log('mode===>', mode);
   return {
     plugins: [
       react(),
       createSvgIconsPlugin({
-        iconDirs: [path.resolve(process.cwd(),'src/assets/icons')],
+        iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
         symbolId: 'icon-[dir]-[name]',
       }),
     ],
     resolve: {
-     alias: {
-      '@': path.resolve('./src')
-     }
-    }
-  }
+      alias: {
+        '@': path.resolve('./src'),
+      },
+    },
+  };
 });
