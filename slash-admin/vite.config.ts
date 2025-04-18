9dea3e115bec43178380cab52afa7eb89ffb3b44
chuxin <15115359055@139.com>
2025-04-18 14:38:14 +0800

    refactor(vite.config.ts): 将配置改为函数形式并添加路径别名
    将 `defineConfig` 从对象形式改为函数形式，以便动态配置。同时添加路径别名 `@` 指向 `src` 目录，提升代码可维护性。

--- slash-admin/vite.config.ts
+++ slash-admin/vite.config.ts
@@ -3,14 +3,19 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
 
-export default defineConfig({
-  plugins: [
-    react(),
-    createSvgIconsPlugin({
-      // 指定需要缓存的图标文件夹
-      iconDir: [path.resolve(process.cwd(), 'src/assets/icons')],
-      // 指定symbolId格式
-      symbolId: 'icon-[dir]-[name]',
-    }),
-  ],
+export default defineConfig(()=>{
+  return {
+    plugins: [
+      react(),
+      createSvgIconsPlugin({
+        iconDirs: [path.resolve(process.cwd(),'src/assets/icons')],
+        symbolId: 'icon-[dir]-[name]',
+      }),
+    ],
+    resolve: {
+     alias: {
+      '@': path.resolve('./src')
+     }
+    }
+  }
 });
